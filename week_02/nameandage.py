997a587af4a68d8418fd7f6c10482c6dac9a93de
Faolán Hamilton <faolanash01@gmail.com>
2025-02-11 23:12:00 +0000

    added an option to nameandage

--- week_02/nameandage.py
+++ week_02/nameandage.py
@@ -2,8 +2,15 @@
 #Reads in a name and age and ouputs this in a clear sentence
 #Author: Faolán Hamilton
 
+'''
 #first, I must define the two key features age and name in quotes
 name = "Faolán"
 age = "23"
 #next, I must print the output sentence, ensuring I have pluses (+) between each piece
 print ("Hello " + name + "," +'\tyour age is '+ age + ".")
+'''
+
+#Or, I can add in the name in the terminal
+name = input("What is your name?")
+age = input ("What is your age?")
+print("Hello " + name + "," +'\tYour age is ' + age + ".")
